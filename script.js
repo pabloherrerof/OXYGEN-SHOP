fe8c4a1db2137a50e6e5f90163b90915572487d7
Pablo Herrero Ferrán <120820455+pabloherrerof@users.noreply.github.com>
2023-03-01 20:09:32 +0100

    Cambios realizados tras la revision del proyecto

---
+++ script.js
@@ -0,0 +1,21 @@
+//BURGUER MENU
+const burger = document.querySelector('.burger i');
+console.log(burger)
+const nav = document.querySelector('.nav');
+
+
+function toggleNav() {
+    burger.classList.toggle('fa-bars');
+    burger.classList.toggle('fa-times');
+    nav.classList.toggle('nav-active');
+}
+
+burger.addEventListener('click', function() {
+    toggleNav();
+});
+
+
+
+
+
+
