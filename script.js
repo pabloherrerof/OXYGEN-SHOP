32ba2cad74c9b42a9360fa0c91a301ff11ff23dd
Pablo Herrero Ferrán <120820455+pabloherrerof@users.noreply.github.com>
2023-03-04 12:26:34 +0100

    cambios

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //BURGUER MENU
 const burger = document.querySelector('.burger i');
 const nav = document.querySelector('.nav');
@@ -38,13 +39,14 @@ eur.addEventListener('click', function () {
 //Llamada a la accion de la funcion changePrice con los parametros de la moneda seleccionada y el valor de cambio.
 async function currencyApi(currency) {
     try {
-        const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${currency}.json`);
+        const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json`);
         if (response.ok) {
             let result = await response.json();
-            changePrice(result[currency], currency)
+            console.log(result)
+            changePrice(result['usd'][currency], currency);
         }
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
@@ -54,7 +56,7 @@ function changePrice(currentValue, currentCurrency) {
         document.getElementById("basic-price").innerHTML = `€0`;
         document.getElementById("profesional-price").innerHTML = `€${Math.round(currentValue * 25)}`;
         document.getElementById("premium-price").innerHTML = `€${Math.round(currentValue * 60)}`;
-        
+
         eur.classList.add("button-active");
         yen.classList.remove("button-active");
         usd.classList.remove("button-active");
@@ -80,10 +82,181 @@ function changePrice(currentValue, currentCurrency) {
 }
 
 
+//FORMULARIO
+document.getElementById("form-button").addEventListener("click", function (event) {
+    event.preventDefault();
+    let nombre = document.getElementById("name");
+    let email = document.getElementById("email");
+    let checkbox = document.getElementById("chbx");
+    let validator = formValidator(nombre.value, email.value, checkbox.checked);
+
+    console.log(validator)
+    if (validator === true) {
+        //POST
+        nombre.classList.remove("error-form");
+        email.classList.remove("error-form");
+        document.getElementById("chbx-terminos").classList.remove("error-form-text");
+
+        postForm(nombre.value, email.value, checkbox.checked);
+    }
+})
+
+function formValidator(nombre, email, checkbox) {
+    let validator = true;
+    if (nombre.length < 2 || nombre.length > 100) {
+        validator = false;
+        document.getElementById("name").placeholder = "Nombre incorrecto (2-100 caracteres)";
+        document.getElementById("name").classList.add("error-form");
+        document.getElementById("name").value = "";
+
+    }
+
+    if ((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) === false)) {
+        validator = false;
+        document.getElementById("email").placeholder = "Email incorrecto";
+        document.getElementById("email").classList.add("error-form");
+        document.getElementById("email").value = "";
+    }
+
+    if (checkbox === false) {
+        validator = false;
+        document.getElementById("chbx-terminos").classList.add("error-form-text");
+    }
+    return validator;
+}
+
+async function postForm(nombre, email, checkbox) {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: nombre,
+            mail: email,
+            acceptedTerms: checkbox
+            
+            
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
 
 
 
 
+//FUNCION SCROLL PERCENTAGE BAR
+
+window.addEventListener('scroll', function () {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("bar").style.width = scrolled + "%";
+})
+
+//TOP BUTTON
+
+window.addEventListener("scroll", function () {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    if (scrolled > 50) {
+        document.getElementById("top-button").style.visibility = "visible";
+    }
+
+    if (scrolled < 50) {
+        document.getElementById("top-button").style.visibility = "hidden";
+    }
+})
+
+document.getElementById("top-button").addEventListener("click", function () {
+    setTimeout(() => {
+        window.scrollTo(0, 0);
+    }, 200)
+})
+
+//MODAL
+window.localStorage.setItem("modalAlreadyShown", "false");
+
+window.addEventListener("scroll", function () {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+
+    if (window.localStorage.getItem("modalAlreadyShown") === "false") {
+        if (scrolled > 25) {
+            document.getElementById("modal").showModal();
+            window.localStorage.setItem("modalAlreadyShown", "true");
+        }
+    }
+})
 
 
+setTimeout(function modalController() {
+    if (window.localStorage.getItem("modalAlreadyShown") === "false") {
+        document.getElementById("modal").showModal();
+        window.localStorage.setItem("modalAlreadyShown", "true");
+    }
+}, 5000);
 
+
+window.addEventListener("keypress", function(event){
+    if(event.key === 'ESC'){
+        document.getElementById("modal").close();
+    }
+})
+
+document.getElementById("modalform-close").addEventListener('click', function(event){    
+    document.getElementById("modal").close();
+    }
+)
+
+document.getElementById("modal-post").addEventListener("click", function(event){
+        event.preventDefault();
+        let nombre = document.getElementById("modalName");
+        let email = document.getElementById("modalEmail");
+        let checkbox = document.getElementById("modal-chbx");
+        let validator = modalFormValidator(nombre.value, email.value, checkbox.checked);
+    
+        console.log(validator)
+        if (validator === true) {
+            //POST
+            nombre.classList.remove("error-form");
+            email.classList.remove("error-form");
+            document.getElementById("modal-terminos").classList.remove("error-form-text");
+    
+            postForm(nombre.value, email.value, checkbox.checked);
+        }
+
+
+})
+
+function modalFormValidator(nombre, email, checkbox) {
+    let validator = true;
+    if (nombre.length < 2 || nombre.length > 100) {
+        validator = false;
+        document.getElementById("modalName").placeholder = "Nombre incorrecto (2-100 caracteres)";
+        document.getElementById("modalName").classList.add("error-form");
+        document.getElementById("modalName").value = "";
+
+    }
+
+    if ((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) === false)) {
+        validator = false;
+        document.getElementById("modalEmail").placeholder = "Email incorrecto";
+        document.getElementById("modalEmail").classList.add("error-form");
+        document.getElementById("modalEmail").value = "";
+    }
+    if (checkbox === false) {
+        validator = false;
+        document.getElementById("modal-terminos").classList.add("error-form-text");
+    }
+    return validator;
+}
+
+
+//Type it prueba
+const modalTitle = document.getElementById("modal-title");
+      
+new TypeIt(modalTitle).go()
